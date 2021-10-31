5e37c4624c6bac713f9e04a7aec67dfaedd59b8d
Jason Galea <jasonpgalea@outlook.com.au>
2021-10-31 18:32:18 +1100

    Refactored into classes, not sure if I'll regret this

--- script/scrape.py
+++ script/scrape.py
@@ -2,7 +2,7 @@
 
 ### Imports
 from logging import error
-import time
+# import time
 from typing import Dict
 from selenium import webdriver
 # from selenium.webdriver.common.keys import Keys
@@ -14,66 +14,59 @@ from mysql.connector import errorcode
 
 
 ### File Imports
-import funcs_general
-import funcs_extract
-import funcs_sql
-from script.funcs_extract import extract_pccg
-from script.funcs_sql import sql_create_database, sql_create_tables, sql_insert_into_hdd, sql_select_all_from_table
+# from funcs_general import ???
+from Extract import Extract
+from SQL import SQL
 
 
-### Globals
-global SQL_HOST, SQL_USER, SQL_PASS, SQL_DB, SQL_TABLE_NAMES, SQL_TABLE_SCHEMAS
-SQL_HOST = "localhost"
-# SQL_HOST = "10.1.1.160"
-SQL_USER = "scraper"
-SQL_PASS = "Password##123"
-SQL_DB = "PriceScraper"
-SQL_TABLE_NAMES = ["HDD", "SSD", "CPU", "GPU"] # AKA. data types/categories
-SQL_TABLE_SCHEMAS = {
-    "HDD":
-        "Time DATETIME\
-        , Retailer varchar(255)\
-        , Title varchar(255)\
-        , URL varchar(255)\
-        , PriceAUD int\
-        , Brand varchar(255)\
-        , Series varchar(255)\
-        , ModelNumber varchar(255)\
-        , HDDCapacity int\
-        , HDDPricePerTB int"
-    , "SSD":
-        "Time DATETIME\
-        , Retailer varchar(255)\
-        , Title varchar(255)\
-        , URL varchar(255)\
-        , PriceAUD int\
-        , Brand varchar(255)\
-        , Series varchar(255)\
-        , ModelNumber varchar(255)\
-        , SSDCapacity int\
-        , SSDPricePerTB int"
-    , "CPU": # TODO: Add unique CPU attributes
-        "Time DATETIME\
-        , Retailer varchar(255)\
-        , Title varchar(255)\
-        , URL varchar(255)\
-        , PriceAUD int\
-        , Brand varchar(255)\
-        , Series varchar(255)\
-        , ModelNumber varchar(255)"
-    , "GPU": # TODO: Add unique GPU attributes
-        "Time DATETIME\
-        , Retailer varchar(255)\
-        , Title varchar(255)\
-        , URL varchar(255)\
-        , PriceAUD int\
-        , Brand varchar(255)\
-        , Series varchar(255)\
-        , ModelNumber varchar(255)"
-}
+### Static Classes
+class Table:
+    NAMES = ["HDD", "SSD", "CPU", "GPU"] # AKA. data types/categories
+    SCHEMAS = {
+        "HDD":
+            "Time DATETIME\
+            , Retailer varchar(255)\
+            , Title varchar(255)\
+            , URL varchar(255)\
+            , PriceAUD int\
+            , Brand varchar(255)\
+            , Series varchar(255)\
+            , ModelNumber varchar(255)\
+            , HDDCapacity int\
+            , HDDPricePerTB int"
+        , "SSD":
+            "Time DATETIME\
+            , Retailer varchar(255)\
+            , Title varchar(255)\
+            , URL varchar(255)\
+            , PriceAUD int\
+            , Brand varchar(255)\
+            , Series varchar(255)\
+            , ModelNumber varchar(255)\
+            , SSDCapacity int\
+            , SSDPricePerTB int"
+        , "CPU": # TODO: Add unique CPU attributes
+            "Time DATETIME\
+            , Retailer varchar(255)\
+            , Title varchar(255)\
+            , URL varchar(255)\
+            , PriceAUD int\
+            , Brand varchar(255)\
+            , Series varchar(255)\
+            , ModelNumber varchar(255)"
+        , "GPU": # TODO: Add unique GPU attributes
+            "Time DATETIME\
+            , Retailer varchar(255)\
+            , Title varchar(255)\
+            , URL varchar(255)\
+            , PriceAUD int\
+            , Brand varchar(255)\
+            , Series varchar(255)\
+            , ModelNumber varchar(255)"
+    }
 
 
-### VARIABLES
+### Variables
 # "header" is not needed, and this one should be Chrome 95 anyway
 # header = {'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
 url = "https://www.pccasegear.com/category/210_344/hard-drives-ssds/3-5-hard-drives"
@@ -105,31 +98,28 @@ soup = bs(driver.page_source, "html.parser")
 
 ### https://dev.mysql.com/doc/connector-python/en/connector-python-example-ddl.html
 # Create connection
-cnx = mysql.connector.connect(
-    host=SQL_HOST
-    , user=SQL_USER
-    , password=SQL_PASS
-    , database=SQL_DB
-)
+
+cnx = SQL.connect()
+
 cursor = cnx.cursor()
-print("Success: Connected to MySQL on {}".format(SQL_HOST))
+print("Success: Connected to MySQL on {}".format(SQL.HOST))
 
 
 # Use/create database
 try:
-    cursor.execute("USE {}".format(SQL_DB))
-    # cnx.database = SQL_DB
+    cursor.execute("USE {}".format(SQL.DB))
+    # cnx.database = SQL.DB
 except mysql.connector.Error as err:
-    print("Failure: Database {} does not exist".format(SQL_DB))
+    print("Failure: Database {} does not exist".format(SQL.DB))
     if err.errno == errorcode.ER_BAD_DB_ERROR:
-        sql_create_database(cursor)
-        print("Success: Database {} created".format(SQL_DB))
-        # cursor.execute("USE {}".format(SQL_DB))
-        cnx.database = SQL_DB
+        SQL.create_database(cursor)
+        print("Success: Database {} created".format(SQL.DB))
+        # cursor.execute("USE {}".format(SQL.DB))
+        cnx.database = SQL.DB
     else:
         print(err.msg) # This error would be non-specific, so I can't describe it beforehand
         exit(1)
-print("Success: Now using database {}".format(SQL_DB))
+print("Success: Now using database {}".format(SQL.DB))
 
 # Drop all tables
 # TODO: Make this conditional
@@ -137,15 +127,15 @@ print("Success: Now using database {}".format(SQL_DB))
 
 # Create tables
 # TODO: Make this a loop, when the schemas for other tables are complete
-sql_create_tables(cursor)
+SQL.create_tables(cursor)
 
 # Extract & insert data into table
-test_data = extract_pccg(soup, 0)
-sql_insert_into_hdd(cursor, test_data)
+test_data = Extract.pccg(soup, 0)
+SQL.insert_into_hdd(cursor, test_data)
 
 
 ### PRINT DATA
-sql_select_all_from_table(0)
+SQL.select_all_from_table(0)
 
 
 ### Close SQL connection
