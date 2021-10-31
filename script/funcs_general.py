e293c66c926a58e5dfc358c3371ca030c6ecae33
Jason Galea <jasonpgalea@outlook.com.au>
2021-10-31 18:02:31 +1100

    Split functions into different files

---
+++ script/funcs_general.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+### Imports
+# from logging import error
+# import time
+# from typing import Dict
+# from selenium import webdriver
+# from bs4 import BeautifulSoup as bs
+
+# import mysql.connector
+# from mysql.connector import errorcode
+
+
+### File Imports
+pass
+
+
+### Functions
