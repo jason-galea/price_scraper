c97a1239e66e03bdd842cc2b03fb9395633fd4b3
Jason Galea <jasonpgalea@outlook.com.au>
2021-10-31 18:32:57 +1100

    Refactored into classes, not sure if I'll regret this

---
+++ script/SQL.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3
+
+### Imports
+from logging import error
+# import time
+# from typing import Dict
+# from selenium import webdriver
+# from bs4 import BeautifulSoup as bs
+
+import mysql.connector
+from mysql.connector import errorcode
+
+
+### File Imports
+from scrape import Table
+
+
+class SQL:
+    # Static variables
+    HOST = "localhost"
+    # HOST = "10.1.1.160"
+    USER = "scraper"
+    PASS = "Password##123"
+    DB = "PriceScraper"
+
+    def connect():
+        return mysql.connector.connect(
+            host=SQL.HOST
+            , user=SQL.USER
+            , password=SQL.PASS
+            , database=SQL.DB
+        )
+
+    def create_database(cursor):
+        try:
+            # No need for "IF NOT EXISTS", because this function is only called to OVERWRITE a corrupt DB? I think?
+            # cursor.execute("CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARACTER SET 'utf8'".format(DB))
+            cursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(SQL.DB))
+            print("Success: Created database {}".format(SQL.DB))
+        except mysql.connector.Error as err:
+            print("Failure: Could not create database: \n{}".format(err.msg))
+            exit(1)
+
+    def drop_tables(cursor):
+        try:
+            cursor.execute("SET FOREIGN_KEY_CHECKS = 0")
+
+            for name in Table.NAMES:
+                cursor.execute("DROP TABLE IF EXISTS {}".format(name))
+                print("Success: Dropped table {}".format(name)) # Will "succeed" even if table was already dropped.
+
+            cursor.execute("SET FOREIGN_KEY_CHECKS = 1")
+        except mysql.connector.Error as err:
+            print("Failure: Could not drop tables: \n{}".format(err.msg))
+            exit(1)
+
+    def create_tables(cursor):
+        for name in Table.NAMES:
+            try:
+                cursor.execute("CREATE TABLE IF NOT EXISTS {} ({})".format(name, Table.SCHEMAS[name]))
+                print("Success: Created table {}".format(name))
+            except mysql.connector.Error as err:
+                if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                    print("Warning: Table {} already exists".format(name))
+                else:
+                    print("Failure: Could not create table {}: \n{}".format(name, err.msg))
+                    # exit(1)
+
+    def insert_into_hdd(cursor, data):
+        # Accepts an array of dicts
+        # Each dict is one row
+        data_type = "HDD" # AKA. table name
+
+        try:
+            for x in data:
+                # I know this is redundant, but it greatly simplifies troubleshooting
+                insert_string = "INSERT INTO {} VALUES('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(
+                    data_type 
+                    , x["Time"]
+                    , x["Retailer"]
+                    , x["Title"]
+                    , x["URL"]
+                    , x["PriceAUD"]
+                    , x["Brand"]
+                    , x["Series"]
+                    , x["ModelNumber"]
+                    , x["HDDCapacity"]
+                    , x["HDDPricePerTB"]
+                )
+                print("\nINSERT STRING:\n{}\n".format(insert_string))
+
+                cursor.execute(insert_string)
+                print("Success: Inserted data into table {}".format(data_type))
+
+        except mysql.connector.Error as err:
+            print("Failure: Could not insert data into table {}: \n{}".format(data_type, err.msg))
+            exit(1)
+
+    def select_all_from_table(table_index):
+        name = Table.NAMES[table_index]
+        pass
