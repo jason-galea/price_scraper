aacdb0cd5c06f431958d72f7e053238ca8de3b0a
Jason Galea <jasonpgalea@outlook.com.au>
2021-11-06 16:57:58 +1100

    Prototype frontend

--- website/index.php
+++ website/index.php
@@ -1,8 +1,63 @@
 <html>
-    <head>
-        <title>PHP Test</title>
-    </head>
-    <body>
-        <?php echo '<p>Hello World</p>'; ?> 
-    </body>
+<head>
+    <title>PHP Test</title>
+</head>
+<body>
+    <header>
+        <h2>
+            Home Page
+        </h2>
+    </header>
+    <?php
+        // Constants
+        $SQL_IP = "127.0.0.1";
+        // $SQL_IP = "10.1.1.60";
+        $SQL_USER = "scraper";
+        $SQL_PASS = "Password##123";
+        $SQL_DB = "PriceScraper";
+
+        // Variables
+        $sqlTable = "HDD";
+
+        $orderColumn = "HDDPricePerTB";
+        $orderDirection = "ASC";
+
+
+        // CONNECT TO MYSQL
+        $con = mysqli_connect($SQL_IP, $SQL_USER, $SQL_PASS);
+        mysqli_select_db($con, $SQL_DB);
+
+
+        // CREATE TABLE
+        echo "<table>";
+        // Fetch column names
+        $result = mysqli_query($con,
+            "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '$table';"
+        );
+        // Insert one row of column names
+        echo "<tr>";
+            foreach ($row as $value) {
+                echo "<td><b>$value</b></td>";
+            } 
+            echo "</tr>";
+        
+        // Fetch contents
+        $result = mysqli_query($con,
+            "SELECT * FROM $sqlTable ORDER BY $orderColumn $orderDirection;"
+        );
+        $row = mysqli_fetch_row($result);
+
+        // Insert all rows of contents
+        while ($row) { // Loop until $row is NULL
+            echo "<tr>";
+            foreach ($row as $value) {
+                echo "<td>$value</td>";
+            } 
+            echo "</tr>";
+            $row = mysqli_fetch_row($result); // Fetch new row
+        }
+
+        echo "</table>";
+    ?> 
+</body>
 </html>
