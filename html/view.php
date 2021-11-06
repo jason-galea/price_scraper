78a99ca8ff0db39fa41049b6f273e52d30e0252e
Jason Galea <jasonpgalea@outlook.com.au>
2021-11-06 20:23:25 +1100

    Restructured, added navigation. TODO: Refactor SQL pipeline

---
+++ html/view.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css"/>
+    
+    <title>Price Scraper</title>
+</head>
+<body>
+    <header>
+        <h2>
+            View Website Data
+        </h2>
+    </header>
+    
+    <?php include_once("nav.php");?>
+
+    <main>
+        <p>
+            <a href="view_graph.php">View data in a graph</a>
+            <a href="view_table.php">View data in a table</a>
+        </p>
+
+        <?php
+            // TODO: Make a "home" menu
+                // 1. Scrape website data
+                    // a. "Which site?"
+                    // b. "Which hardware category?"
+                    // c. --> Pass arguments to script                        
+                        // WHEN IMPLEMENTED, THE SCRIPT CAN BE MANAGED ENTIRELY FROM THE SITE
+                // 2. View website data
+                    // a. "Graph?"
+                        // "Select metric"
+                            // Eg. Price, Capacity, Price per TB
+                    // b. "Table?"
+                        // "Select ???"
+                    // c. "Clear table?"
+                        // WHEN IMPLEMENTED, THE SCRIPT CAN BE MANAGED ENTIRELY FROM THE SITE
+
+
+            // asd
+        ?>
+    </main>
+</body>
