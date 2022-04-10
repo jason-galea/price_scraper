597e3bae496d69f60d1b46f35604c2c701c762aa
Jason Galea <jasonpgalea@outlook.com.au>
2022-04-10 18:15:01 +1000

    test

---
+++ git_commit.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+git add -A
+echo
+git status
+echo
+
+read -p "Enter commit message: " commit_message
+
+git commit -m $commit_message
+
+echo "git pull"
+echo
+git push
+echo
+
+read -p "Continue to push commit? (^C to cancel)"
+
+echo "git push"
+echo
+echo
+
