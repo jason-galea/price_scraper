729595f4a6f540db77512eeca9a4de1c05d4e078
JG <jasongalea98@outlook.com>
2023-03-24 21:46:34 +1100

    Added service

--- install_service.sh
+++ install_service.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo -e "\n==> Link service to /etc/systemd/system/price_scraper.service"
-sudo ln -s ./misc/price_scraper.service /etc/systemd/system/price_scraper.service
+echo -e "\n==> Copy service to /etc/systemd/system/price_scraper.service"
+sudo cp ./misc/price_scraper.service /etc/systemd/system/price_scraper.service
 
 echo -e "\n==> Enable service"
 sudo systemctl enable --now price_scraper
