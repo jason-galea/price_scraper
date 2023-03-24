2b6caedec639138b393601bf3144bc8c39afd38b
JG <jasongalea98@outlook.com>
2023-03-24 22:02:01 +1100

    Update common functions

--- lib/common.py
+++ lib/common.py
@@ -5,20 +5,23 @@ import json
 
 
 ### COMMON FUNCTIONS
-def concaternate_items_within_list(l, i, j):
-    ### Receives list & two indexes
-    ### Returns list with elements between the indexes concaternated (inclusive)
-    ### E.G: concaternate_items_within_list(["a", "b", "c", "d"], 1, 2) --> ["a", "b c", "d"]
-    for _ in range(j - i):
-        l[i] = f"{l[i]} {l[i + 1]}"
-        del l[i + 1]
+def concaternate_items_within_list(input_list: list, start_index: int, end_index: int) -> list:
+    """
+    Receives list & two indexes.\n
+    Returns list with elements between the indexes concaternated (inclusive).\n
+    E.G: concaternate_items_within_list(["a", "b", "c", "d"], 1, 2) --> ["a", "b c", "d"]
+    """
+    result_list = input_list
+    for _ in range(end_index - start_index):
+        result_list[start_index] = f"{input_list[start_index]} {input_list[start_index + 1]}"
+        del result_list[start_index + 1]
 
-    return l
+    return result_list
 
-def remove_multiple_strings_from_list(l=list, strings_to_remove=list):
+def remove_multiple_strings_from_list(l: list, strings_to_remove: list) -> list:
     return [s for s in l if (s not in strings_to_remove)]
 
-def export_json(extracted_data, dir, file):
+def export_json(extracted_data: iter, dir: str, file: str) -> None:
     print(f"\nExporting data to {file}\n")
 
     ### Check/Create dir
