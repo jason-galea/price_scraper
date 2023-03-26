d11ad936c4840d67d108cd2516c2ce4474e9303d
Jason Galea <jasonpgalea@outlook.com>
2023-03-26 23:22:25 +1100

    Better method to dodge scorptec bot detection

--- lib/Scorptec.py
+++ lib/Scorptec.py
@@ -2,7 +2,7 @@ import os
 # import datetime
 # import enum
 import json
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 # from selenium import webdriver
 # from selenium.webdriver import Firefox, DesiredCapabilities
 # from selenium.webdriver.firefox.options import Options
@@ -28,54 +28,66 @@ class Scorptec:
 
         ### Pagination fun!!!1!!!!
         ### 90 items per page
-        select_dropdown = Select(driver.find_element(By.ID, "pagination-view-count"))
-        select_dropdown.select_by_visible_text("90")
+        # select_dropdown = Select(driver.find_element(By.ID, "pagination-view-count"))
+        # select_dropdown.select_by_visible_text("90")
         
-        ### Sort by price, low to high
-        sort_dropdown = Select(driver.find_element(By.ID, "widget-sort"))
-        sort_dropdown.select_by_visible_text("Price (low to high)")
+        # ### Sort by price, low to high
+        # sort_dropdown = Select(driver.find_element(By.ID, "widget-sort"))
+        # sort_dropdown.select_by_visible_text("Price (low to high)")
 
-        last_page = driver.find_element(By.ID, "total-page").text
-        # print(f"last_page = {last_page}")
+        last_page = int(driver.find_element(By.ID, "total-page").text)
+        # last_page = None
+        # product_elements = []
+        bs4_hmtl_parsers = []
 
+        ### TODO: Refactor so pages are fetched within the iteration they are parsed
         while True:
+            # ### First iteration, find last_page and skip repeat download
+            # if (not last_page):
+            #     last_page = int(driver.find_element(By.ID, "total-page").text)
+            # else:
+
             ### Find current page
-            print(f"==> INFO: Attempting to find current page")
-            # current_page = int(driver.find_element(By.ID, "current-page").text)
-            # current_page_element = WebDriverWait(driver, 30).until(
-            #     EC.presence_of_element_located((By.ID, "current-page"))
-            # )
+            # print(f"==> INFO: Attempting to find current page")
             current_page_element = WebDriverWait(driver, 10).until(
                 lambda x: x.find_element(By.ID, "current-page")
             )
             current_page = int(current_page_element.text)
+            # current_page = int(driver.find_element(By.ID, "current-page").text)
             print(f"==> INFO: Opened page {current_page}/{last_page}")
 
             ### Print product URLs
+            count = 0
             for element in driver.find_elements(
                 by=By.XPATH,
-                value="//div[@class='detail-product-title']/a",
+                value="//div[@class='detail-product-title']/a[contains(@href,'product') and @class='inherit-class']",
             ):
-                href = element.get_attribute("href").strip()
-                if (href != ""):
-                    print(f"href = {href}")
+                count += 1
+                # product_elements.append(element)
+                print(f"element.text = '{element.text}'")
+            print(f"==> INFO: Found {count} products on page {current_page}/{last_page}")
+
+            ### Save HTML parser objects
+            bs4_hmtl_parsers.append(
+                BeautifulSoup(
+                    markup=driver.page_source,
+                    features="html.parser"
+                )
+            )
 
             ### Go to next page (somehow)
-            # if (current_page != last_page):
-            if (current_page < int(last_page) - 1):
-                # driver.find_element(By.CLASS_NAME, "pagination-next").click()
-                # driver.execute_script("showNextPage();return false;");
-                print(f"==> INFO: Attempting to load {self.CATEGORY_URLS[category]}?page={current_page + 1}")
+            # if (current_page < int(last_page) - 1):
+            if (current_page != last_page):
+                # print(f"==> INFO: Attempting to load {self.CATEGORY_URLS[category]}?page={current_page + 1}")
                 
-                # driver = instantiate_ff_driver_and_download(self.CATEGORY_URLS[category])
-
-                # driver.quit()
-                driver.close()
-                # del driver
-                driver = instantiate_ff_driver_and_download(f"{self.CATEGORY_URLS[category]}?page={current_page + 1}")
+                # driver.close() ### Workaround for anti-bot nonsense
+                driver.delete_all_cookies() ### Workaround for anti-bot nonsense
+                next_page_url = f"{self.CATEGORY_URLS[category]}?page={current_page + 1}"
+                # driver = instantiate_ff_driver_and_download(next_page_url)
+                driver.get(next_page_url)
             else:
+                # print(f"==> INFO: Found a total of {len(product_elements)} product elements")
                 break
-            # break
 
         return
 
@@ -100,7 +112,7 @@ class Scorptec:
 
 
     ### TODO: Move this into base class/common function
-    def _extract(self, category, bs4_html_parser: bs) -> list:
+    def _extract(self, category, bs4_html_parser: BeautifulSoup) -> list:
         match category:
             case "hdd":
                 return self._extract_hdd_data(bs4_html_parser)
