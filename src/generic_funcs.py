e3fe719bb2feeeff13595d60a6660b3f8ed378cb
Jason Galea <jasonpgalea@outlook.com.au>
2024-01-05 02:35:13 +1100

    chore: cleanup

--- src/generic_funcs.py
+++ src/generic_funcs.py
@@ -1,7 +1,5 @@
 import datetime
 
-# def get_json_filenames() -> list:
-#     return glob(f"{JSON_OUTPUT_DIR}/*.json")
 
 def list_contains_all_values(haystack, needles) -> bool:
     return all((n in haystack) for n in needles)
@@ -18,4 +16,3 @@ def get_iso_8601_time(dt: datetime.datetime):
     Convert any datetime object to ISO 8601 standard format
     """
     return dt.strftime('%Y-%m-%dT%H:%M:%S.%f%z')
-
